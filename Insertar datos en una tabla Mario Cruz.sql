4b91a2817c25f1707e21182e42de5f3eec524e3a
mario18acp-lang <mario.18acp@gmail.com>
2026-03-12 23:53:00 -0600

    Add files via upload

---
+++ Insertar datos en una tabla Mario Cruz.sql
@@ -0,0 +1,10 @@
+--- INSERTAR DATOS EN TABLA
+
+insert into datos values ('1', 'Mario','123456','Raul_Zarate');
+
+insert into datos (id,nombre,direccion, cedula_profesional) values
+('2', 'Mariana Gazcon', 'camino san blas tepic', 'X2432'),
+('3', 'Sebatian Angeles', 'camino real de calacoya', '4077'),
+('4', 'Karen Zetina', 'boulevares de lago', '22783');
+
+select * from datos;
